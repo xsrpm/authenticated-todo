71f873fe588c59bc2c89ada6dbd51ce73b8ad5ed
César Palma Morante <cemp2703@gmail.com>
2022-08-17 16:59:06 -0500

    setup inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Button  from '@mui/material/Button'
+
+function App() {
+  return (
+    <div className="App">
+     <Button variant="contained" >Hola</Button>
+    </div>
+  )
+}
+
+export default App
