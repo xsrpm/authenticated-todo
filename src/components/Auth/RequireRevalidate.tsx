0952ee85bbd37d13a8cd14b08e06706a9515e045
César Palma Morante <cemp2703@gmail.com>
2023-01-25 20:26:26 -0500

    fix reset password config

--- src/components/Auth/RequireRevalidate.tsx
+++ src/components/Auth/RequireRevalidate.tsx
@@ -1,19 +1,18 @@
 import {useEffect} from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
 export function RequireRevalidate({ children }: { children: JSX.Element }){
     const navigate = useNavigate()
     const {getUser} = useAuth()
-    const user = getUser()
     
     useEffect(()=>{
         setTimeout(()=>{
-            if(!user) navigate("/signin")
+            if(!getUser()) navigate("/signin")
         }, 2000)
     },[])
     
-    if(!user) return <h1>Loading</h1>
+    if(!getUser()) return <h1>Loading</h1>
     
     return children;
 }
